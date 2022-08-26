5f4362d808200607c41739e756731c96da1fa989
heidegomes <heidegc@hotmail.com>
2022-08-26 17:13:55 -0300

    1ª tentativa de resolver requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalActual = species.filter((bicho) => (bicho.name === animal)); // retorna um array com o objeto da especie
+  console.log(animalActual);
+
+  const animalsResidents = animalActual.map((bicho2) => (bicho2.residents)); // retorna um array com o objeto do animais residentes
+  console.log(animalsResidents);
+
+  const verifyAge = animalsResidents.every((bicho3) => (bicho3.age === age || bicho3.age > age)); // verifica se a idade de todos os residentes atendem a condição
+  console.log(verifyAge);
+
+  return verifyAge;
 }
+// console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  
+  
+  return [{
+    id,
+    name,
+    popularity,
+    location,
+    availability,
+    residents:
+      { name, sex, age },
+  }];
+ 
 }
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
