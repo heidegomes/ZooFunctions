2db794f66658744b4a8323126d31d6a841470b1b
heidegomes <heidegc@hotmail.com>
2022-08-29 17:42:18 -0300

    Requisito 2 feito!

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,21 +3,12 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const animalActual = species.filter((bicho) => (bicho.name === animal)); // retorna um array com o objeto da especie
-  console.log(animalActual);
 
   const residents = animalActual[0].residents;
-  
 
-  console.log(residents);
-
-  // const animalsResidents = animalActual.map((bicho2) => (bicho2.residents)); // retorna um array com o objeto do animais residentes
-  // console.log(animalsResidents);
-
-  // const verifyAge = animalsResidents.every((bicho3) => (bicho3.age >= age)); // verifica se a idade de todos os residentes atendem a condição
-  // console.log(verifyAge);
-
-  // return verifyAge;
+  const verifyAge = residents.every((bicho) => bicho.age >= age);
+  return verifyAge;
 }
-getAnimalsOlderThan('penguins', 10);
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
