8db9759e72afd2a56ea995e55ce3a067b6e6ccc1
heidegomes <heidegc@hotmail.com>
2022-08-31 10:10:41 -0300

    Requisito 8 feito!

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,14 @@
 const data = require('../data/zoo_data');
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 // const { hours } = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  if (scheduleTarget === species.name) {
-    return species.availability;
-  }
-  if (scheduleTarget === undefined) {
-  }
-  if (scheduleTarget !== species.name ||)
+  // if (scheduleTarget === species.name) {
+  //   return species.availability;
+  // }
+  // if (scheduleTarget === undefined) {
+  // }
+  // if (scheduleTarget !== species.name ||)
 }
-
 
 module.exports = getSchedule;
